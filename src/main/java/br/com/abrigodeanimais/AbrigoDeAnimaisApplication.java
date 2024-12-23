4168d46cdee0a73559f625df4a203a10c068dcd0
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-12-23 17:24:18 -0300

    feat: configuracao inicial

---
+++ src/main/java/br/com/abrigodeanimais/AbrigoDeAnimaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.abrigodeanimais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrigoDeAnimaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbrigoDeAnimaisApplication.class, args);
+	}
+
+}
